6e5caf297f16419c3dec267277b1f360d7b07378
Yuto Nakamura <yutonakamura0320@gmail.com>
2018-09-30 17:23:13 +0900

    Initial Commit

---
+++ Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Yuto Nakamura on 2018/09/30.
+//  Copyright © 2018 Yuto Nakamura. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
